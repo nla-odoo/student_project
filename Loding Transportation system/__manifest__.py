dca36921fb1c7918e01e2e6f4cfbce7b7eac5297
sahista92 <heena92pathan@gmail.com>
2020-04-10 11:06:21 +0530

    lts

--- Loding Transportation system/__manifest__.py
+++ Loding Transportation system/__manifest__.py
@@ -13,17 +13,8 @@
 
 
     'data': [
-        'security/security.xml',
         'security/ir.model.access.csv',
-        # 'views/transporters_views.xml',
-        # 'views/drivers_views.xml',
-        # 'views/inquiries_views.xml',
-        # 'views/orders_views.xml',
-        # 'views/vehicles_views.xml',
-        # 'views/reports.xml',
-        # 'views/template.xml',
         'views/project_temp.xml',
-        'views/sysparam.xml',
 
     ],
     'demo': ['demo/demo.xml', 'demo/demo_project.xml', 'demo/order_demo.xml', ],
--- Loding Transportation system/models/sysparam.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class example(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    merchant_id = fields.Char(config_parameter="MID")
-    merchant_key = fields.Char(config_parameter="MERCHANT_KEY")

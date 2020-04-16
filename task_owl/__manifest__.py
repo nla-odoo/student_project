3d3c8a84e5b45b18248d18326d62423a80373f5a
YOGRAJ TANDEL <yograjtandel9@gmail.com>
2020-04-16 14:21:34 +0530

    [add] task using owl

---
+++ task_owl/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "task_owl",
+    'depends': [],
+    'author': 'Tandel Yograj ',
+    'category': 'Category',
+    'description': """Manage & track vehicles in business like travells,transportation etc,""",
+    'data': [
+            ],
+
+    'application': True
+}

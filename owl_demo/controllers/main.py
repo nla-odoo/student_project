a9feb21459369a33aa908317fe061d9861561008
Nikunj Ladava <nla@odoo.com>
2020-04-28 18:46:02 +0530

    [ADD] owl_dynamic_demo

--- owl_demo/controllers/main.py
+++ owl_demo/controllers/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import request
 
 
 class OwlController(http.Controller):
@@ -7,3 +8,7 @@ class OwlController(http.Controller):
     @http.route('/owl_demo', type='http', auth="public", csrf=False)
     def owl_demo(self, **post):
         return http.request.render("owl_demo.demo_template")
+
+    @http.route('/get_partner_data', type='json', auth="public", csrf=False)
+    def get_partner(self, **post):
+        return request.env['res.partner'].search([]).mapped('name')

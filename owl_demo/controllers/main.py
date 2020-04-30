842d31c97e6c0cd1a0a2086b6fc6e41e451328aa
vishal5897 <anuch983@gmail.com>
2020-04-30 23:22:04 +0530

    owl pagination but chek logic sir

--- owl_demo/controllers/main.py
+++ owl_demo/controllers/main.py
@@ -5,13 +5,21 @@ from odoo.http import request
 
 class OwlController(http.Controller):
 
-    @http.route('/owl_demo', type='http', auth="public", csrf=False)
+    @http.route('/owl_demo', type='http', auth="public", csrf=False, website=True)
     def owl_demo(self, **post):
-        products = request.env['product.template'].sudo().search([])
-        print("\n\n", products)
+        return http.request.render("owl_demo.demo_template")
 
-        return http.request.render("owl_demo.demo_template", {"products": products})
-
-    @http.route('/get_partner_data', type='json', auth="public", csrf=False)
+    @http.route('/get_partner_data', type='json', auth="public", csrf=False, website=True)
     def get_partner(self, **post):
-        return request.env['res.partner'].search([]).mapped('name')
+        product_list = []
+        products = request.env['product.template'].sudo().search([])
+        for product in products:
+            product_list.append({
+                "id": product.id,
+                "name": product.name,
+                "price": product.list_price,
+                "type": product.type,
+                "image": product.image_1024
+            })
+            print(product.list_price, "\n\n")
+        return product_list

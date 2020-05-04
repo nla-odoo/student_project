fdc38a3c5c8bde2974fcb273c6a9d7770065147b
Manisha5897 <11708111.gvp@gujaratvidyapith.org>
2020-05-04 15:31:25 +0530

    add btn component

--- owl_work/static/src/js/btn_component.js
+++ owl_work/static/src/js/btn_component.js
@@ -7,12 +7,15 @@ odoo.define('owl_work.btn_component', function(require){
     const { xml } = owl.tags;
 
     const btn_temp = xml`
-        <button t-on-click="state.value">
+        <button id="btn" t-on-click="_onClickButton">
         Help Chat Board
         </button>`;
 
     class OwlWork extends Component{
-        state = useState({ value: "Helllooooo"})
+        state = useState({ value: "Helllooooo" })
+        _onClickButton(){
+            alert(this.state.value);
+        }
     }
     OwlWork.template = btn_temp;
 

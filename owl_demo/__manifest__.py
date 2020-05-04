e92f5af319dff0934302fef1c70476608332b96a
sahista92 <heena92pathan@gmail.com>
2020-05-04 15:28:37 +0530

    owl_product

---
+++ owl_demo/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Owl Demo',
+    'summary': 'Owl Demo',
+    'depends': ['base', 'portal', 'sale_management', ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}

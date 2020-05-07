c69225cb140c049b09fb6804412084555c4bf772
Manisha5897 <11708111.gvp@gujaratvidyapith.org>
2020-05-07 16:59:16 +0530

    open chat window

--- owl_work/controllers/main.py
+++ owl_work/controllers/main.py
@@ -3,6 +3,6 @@ from odoo import http
 
 
 class OwlController(http.Controller):
-    @http.route('/owl_work', type='http', auth='public', csrf=False)
+    @http.route('/owl_work', type='http', auth='public', csrf=False, website=True)
     def owl_work(self, **post):
         return http.request.render("owl_work.btn_template")
--- owl_work/static/src/js/btn_component.js
+++ owl_work/static/src/js/btn_component.js
@@ -1,23 +1,63 @@
 odoo.define('owl_work.btn_component', function(require){
     "use strict";
 
+    var session = require('web.session');
+    var WebsiteLivechat = require('im_livechat.model.WebsiteLivechat');
+    var WebsiteLivechatMessage = require('im_livechat.model.WebsiteLivechatMessage');
+    var WebsiteLivechatWindow = require('im_livechat.WebsiteLivechatWindow');
+         
     require('web.dom_ready');
 
     const { Component, useState, hooks } = owl;
     const { xml } = owl.tags;
 
-    const btn_temp = xml`
-        <button id="btn" t-on-click="_onClickButton">
-        Help Chat Board
-        </button>`;
-
     class OwlWork extends Component{
-        state = useState({ value: "Helllooooo" })
-        _onClickButton(){
-            alert(this.state.value);
+        static template = xml`
+            <button t-on-click="_onClickButton">
+                Click Me
+            </button>`;
+        state = useState({ value: "Helllooooo"})
+
+        _livechat = null;
+
+        _openChatWindow() {
+            var self = this;
+            var options = {
+                displayStars: false,
+                headerBackgroundColor: 'black',
+                placeholder: 'please enter msg',
+                titleColor: 'white',
+            };
+            this._chatWindow = new WebsiteLivechatWindow(this, this._livechat, options);
+            return this._chatWindow.appendTo($('body')).then(function () {
+                var cssProps = {bottom: 0};
+                self._chatWindow.$el.css(cssProps);
+                $(self.el).hide();
+            });
+        }
+        _onClickButton() {
+            // var button = new im_livechat.LivechatButton(rootWidget);
+            // button._openChat();
+            const self = this;
+            let def;
+            def = session.rpc('/im_livechat/get_session', {
+                channel_id : 1,
+                anonymous_name : 'Visitor',
+                previous_operator_id: '3',
+            }, {shadow: true});
+            def.then(function (livechatData) {
+                if (!livechatData || !livechatData.operator_pid) {
+                    return;
+                } else {
+                    self._livechat = new WebsiteLivechat({
+                        parent: self,
+                        data: livechatData
+                    });
+                    return self._openChatWindow().then(function () {});
+                }
+            });
         }
     }
-    OwlWork.template = btn_temp;
 
     const workInstance = new OwlWork();
     workInstance.mount(document.body);

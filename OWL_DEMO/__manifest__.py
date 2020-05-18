d4455379b7aa926437904bb75432859712c09f4b
Mithilesh Rao <mithileshrao1995@gmail.com>
2020-05-18 10:42:45 +0530

    new Commit

--- OWL_DEMO/__manifest__.py
+++ OWL_DEMO/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'OWL DEMO',
-    'summary': 'OWL DEMO',
-    'depends': ['base', 'portal', 'rating', ],
+    'name': 'OWL_DEMO',
+    'summary': 'OWL_DEMO',
+    'depends': ['base', 'portal', 'rating', 'sale_management'],
     'data': [
         'views/templates.xml',
     ],
--- OWL_DEMO/static/src/js/owl_rating_component.js
+++ OWL_DEMO/static/src/js/owl_rating_component.js
@@ -13,37 +13,73 @@ odoo.define('OWL_DEMO.owl_rating_component', function (require) {
     const { xml } = owl.tags;
     const { whenReady } = owl.utils;
 
+
+
     class OwlRating extends Component {
         constructor() {
             super(...arguments);
             this.state = useState({
-                res_id: "",
+                rating: "",
                 feedback:"",
+                res_id:"",
+                res_model:"",
             });
         }
-    async _onClickLink(ev) {
-            const self = this;
-            rpc.query({ route: "/owl_demo_rating",
-                params:{res_id: this.state.feedback,
-                feedback: this.state.feedback,
-            }}).then(function (result) {
-                self.render(true);
-                self.env.qweb.forceUpdate();
-            });
+
+        async willStart() {
+            this.product = await this.getProduct();
         }
+
+        async getProduct () {
+           return this.service;
+        }
+        get service ()  {
+            debugger
+            return this.product;
+        }
+
+        async _onClickLink(ev) {
+            this.product = await rpc.query({ route: "/owl_demo_rating", 
+                params:{rating: this.state.rating, 
+                    feedback: this.state.feedback,
+                    res_id: this.state.res_id,
+                    res_model: this.state.res_model
+                }});
+            this.render(true);
+          
+        }
+
+    // async _onClickLink(ev) {
+    //         const self = this;
+    //         rpc.query({ route: "/owl_demo_rating",
+    //             params:{res_id: this.state.feedback,
+    //             feedback: this.state.feedback,
+    //         }}).then(function (result) {
+    //             self.render(true);
+    //             self.env.qweb.forceUpdate();
+    //         });
+    //     }
 
          static template = xml`<div>
             <div>
             <div>
                 <form method="post">
-                    <center><h1>Create Inquirey</h1></center>
+                    <center><h1>Complain</h1></center>
                     <div class="form-group">
-                        <label>Feedback</label>
-                        <input type="number" name='res_id' t-model="state.res_id" class="form-control"/>
+                        <label>Rate</label>
+                        <input type="number" name='rate' t-model="state.rating" class="form-control"/>
                     </div>
                     <div class="form-group">
-                        <label>Feedback</label>
+                        <label>feedback</label>
                         <input type="text" name='feedback' t-model="state.feedback" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Res-id</label>
+                        <input type="number" name='res_id' t-model="state.res_id" class="form-control"/>
+                    </div>
+                     <div class="form-group">
+                        <label>res_model</label>
+                        <input type="text" name='res_model' t-model="state.res_model" class="form-control"/>
                     </div>
                 <a t-on-click="_onClickLink" class="btn btn-primary">Submit</a>
                 </form>
